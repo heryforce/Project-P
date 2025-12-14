a314b53b9e77f6e3089ff179a54fe43bcd58278d
Yasko <yacineivombopro@gmail.com>
2025-12-14 17:56:03 +0100

    main menu

---
+++ main_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+
+
+func _on_load_game_button_pressed() -> void:
+	print("Chargement de la partie'zer...")
+
+
+func _on_options_game_button_pressed() -> void:
+	print("Bienvenue Morray, bientôt disponible")
+
+
+func _on_quit_game_button_pressed() -> void:
+	get_tree().quit()
